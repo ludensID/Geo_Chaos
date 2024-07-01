600df55c13da8bf4f6dc0ea938fc687b56aa2064
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-07-01 14:38:41 +0300

    update: Rename configs for compliance of naming rules

--- GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Constants/CAC.cs
+++ GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Constants/CAC.cs
@@ -10,29 +10,29 @@
 
     public static class Names
     {
-      public const string HERO_FILE = "Hero Config";
       public const string HERO_MENU = CONFIG_MENU + "Hero";
+      public const string HERO_FILE = "HeroConfig";
 
-      public const string INPUT_ACTION_NAME_FILE = "Input Action Name Map";
       public const string INPUT_ACTION_NAME_MENU = CONFIG_MENU + "Input Action Name Map";
+      public const string INPUT_ACTION_NAME_FILE = "InputActionNameMap";
 
-      public const string CONFIG_PROVIDER_FILE = "Config Provider";
       public const string CONFIG_PROVIDER_MENU = CONFIG_MENU + "Config Provider";
+      public const string CONFIG_PROVIDER_FILE = "ConfigProvider";
 
-      public const string PHYSICS_FILE = "Physics Config";
       public const string PHYSICS_MENU = CONFIG_MENU + "Physics";
+      public const string PHYSICS_FILE = "PhysicsConfig";
 
-      public const string PREFAB_FILE = "Prefab Provider";
       public const string PREFAB_MENU = CONFIG_MENU + "Prefabs";
+      public const string PREFAB_FILE = "PrefabProvider";
 
-      public const string SHARD_POOL_FILE = "Shard Pool Config";
       public const string SHARD_POOL_MENU = CONFIG_MENU + "Shard Pool";
+      public const string SHARD_POOL_FILE = "ShardPoolConfig";
       
       public const string LAMA_MENU = CONFIG_MENU + "Lama";
-      public const string LAMA_FILE = "Lama Config";
+      public const string LAMA_FILE = "LamaConfig";
       
       public const string SPIKE_MENU = CONFIG_MENU + "Spike";
-      public const string SPIKE_FILE = "Spike Config";
+      public const string SPIKE_FILE = "SpikeConfig";
     }
   }
 }
