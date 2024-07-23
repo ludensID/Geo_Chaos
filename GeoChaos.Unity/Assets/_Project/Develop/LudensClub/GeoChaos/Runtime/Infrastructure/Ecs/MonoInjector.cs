45558cdad275785e45f4a72290605029b820d5d7
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-07-23 23:21:32 +0300

    feat: Check that game object will not be injected in hierarchy

--- GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Infrastructure/Ecs/MonoInjector.cs
+++ GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Infrastructure/Ecs/MonoInjector.cs
@@ -4,6 +4,7 @@ using Zenject;
 
 namespace LudensClub.GeoChaos.Runtime.Infrastructure
 {
+  [DisallowMultipleComponent]
   [AddComponentMenu(ACC.Names.MONO_INJECTOR)]
   public class MonoInjector : MonoBehaviour, IInjectable
   {
@@ -17,7 +18,30 @@ namespace LudensClub.GeoChaos.Runtime.Infrastructure
 
     private void Awake()
     {
-      this.EnsureInjection();
+      if (!Injected)
+      {
+        Context ctx = GetComponentInParent<GameObjectContext>(true);
+        var parentInjector = transform.parent.GetComponentInParent<MonoInjector>(true);
+
+        switch ((bool)ctx, (bool)parentInjector)
+        {
+          case (false, false):
+          case (false, true) when parentInjector.Injected:
+          case (true, false):
+          case (true, true) when parentInjector.transform.IsChildOf(ctx.transform) && parentInjector.Injected:
+          case (true, true) when ctx.transform.IsChildOf(parentInjector.transform):
+          case (true, true) when ctx.gameObject == parentInjector.gameObject && parentInjector.Injected:
+            Inject(ctx);
+            break;
+        }
+      }
+    }
+
+    private void Inject(Context ctx)
+    {
+      if (ctx == null)
+        ctx = FindAnyObjectByType<SceneContext>();
+      ctx.Container.InjectGameObject(gameObject);
     }
   }
 }
