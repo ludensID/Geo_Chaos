e489566d4d9238778430df6ad177d6b59154b3d3
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-08-03 13:13:05 +0300

    feat: Add validate entity button to converter

--- GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Infrastructure/Ecs/Converters/EcsConverterAsset.cs
+++ GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Runtime/Infrastructure/Ecs/Converters/EcsConverterAsset.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Leopotam.EcsLite;
 using TriInspector;
 using UnityEngine;
 
@@ -9,7 +11,7 @@ namespace LudensClub.GeoChaos.Runtime.Infrastructure.Converters
   public class EcsConverterAsset : ScriptableObject, IEcsConverter
   {
     public List<EcsConverterValue> Converters;
-      
+
     public void ConvertTo(EcsEntity entity)
     {
       foreach (EcsConverterValue converter in Converters)
@@ -21,5 +23,26 @@ namespace LudensClub.GeoChaos.Runtime.Infrastructure.Converters
     public void ConvertBack(EcsEntity entity)
     {
     }
+
+#if UNITY_EDITOR
+    [Button(ButtonSizes.Medium, "Validate Entity")]
+    [PropertyOrder(0)]
+    [PropertySpace(SpaceAfter = 10)]
+    [GUIColor(0f, 1f, 1f)]
+    private void ValidateEntity()
+    {
+      var world = new EcsWorld();
+      EcsEntity entity = world.CreateEntity();
+      try
+      {
+        ConvertTo(entity);
+        Debug.Log("Entity was successfully created");
+      }
+      catch (Exception e)
+      {
+        Debug.LogException(e);
+      }
+    }
+#endif
   }
 }
