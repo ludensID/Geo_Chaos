7d9693a4083b9c3672edad2ee8d484b0b0abd339
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-10-10 17:24:21 +0300

    feat: Add post processing for tile platform

---
+++ GeoChaos.Unity/Assets/_Project/Develop/LudensClub/GeoChaos/Editor/LDtk/TilePlatformPostProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using LDtkUnity;
+using LDtkUnity.Editor;
+using UnityEngine;
+
+namespace LudensClub.GeoChaos.Editor.LDtk
+{
+  public class TilePlatformPostProcessor : LDtkPostprocessor
+  {
+    protected override void OnPostprocessLevel(GameObject root, LdtkJson projectJson)
+    {
+      var level = root.GetComponent<LDtkComponentLevel>();
+      LDtkComponentLayer platformLayer = level.LayerInstances.FirstOrDefault(x => x && x.Identifier == "Platforms");
+      if (platformLayer == null)
+        return;
+
+      foreach (LDtkComponentEntity entity in platformLayer.EntityInstances)
+      {
+        if (entity.transform.childCount > 0)
+        {
+          Vector3 size = entity.transform.localScale;
+
+          Transform child = entity.transform.GetChild(0);
+          var sprite = child.GetComponent<SpriteRenderer>();
+          sprite.size = size / 2;
+
+          size.x = 1;
+          size.y = 1;
+          entity.transform.localScale = size;
+        }
+      }
+    }
+  }
+}
